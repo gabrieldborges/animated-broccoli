fe70be48913f7f073ec2c494123d98f8c8cb4bcf
Gabriel Dantas <gabrieldantas1404@icloud.com>
2025-12-15 17:35:44 -0300

    Add factory pattern of organization

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, jsonify , request
 from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
+from src.main.factories.calculator_2_factory import calculator_2_factory
+from src.main.factories.calculator_1_factory import calculator_1_factory
+
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=['POST'])
 def calculator_1() :
-    calc = Calculator1()
+    calc = calculator_1_factory()
     result = calc.calculate(request)
  
     return jsonify(result), 200
@@ -16,8 +17,8 @@ def calculator_1() :
 
 @calc_route_bp.route("/calculator/2", methods=['POST'])
 def calculator_2() :
-    driver = NumpyHandler()
-    calc = Calculator2(driver)
+    calc = calculator_2_factory()   
+
     result = calc.calculate(request)
  
     return jsonify(result), 200
