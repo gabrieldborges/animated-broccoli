90df248a7469462a1116f61302dcbe84f36e75a2
Gabriel Dantas <gabrieldantas1404@icloud.com>
2025-12-15 00:14:03 -0300

    Add initial server setup and calculator route

---
+++ src/main/server/server.py
@@ -0,0 +1,10 @@
+# Definições báscias para o servidor
+
+from flask import Flask
+from src.main.routes.calculators import calc_route_bp
+
+
+app = Flask(__name__)   
+    
+app.register_blueprint(calc_route_bp)
+
